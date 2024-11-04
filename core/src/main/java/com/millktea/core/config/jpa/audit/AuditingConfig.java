fd6015d0d5ad7273f24e0b77f1959bf8ccc8b9ea
thom-droid <thom-droid@github.com>
2024-11-04 22:00:13 +0900

    setup: jpa auditing 추가

---
+++ core/src/main/java/com/millktea/core/config/jpa/audit/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.millktea.core.config.jpa.audit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditingConfig {
+}
